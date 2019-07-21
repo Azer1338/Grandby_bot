1941ee58050296ee8983d4298426707d683fd606
Adrien1338 <adrien13.f@gmail.com>
2019-07-21 17:55:37 +0200

    MediaWiki library added in the project. Everything works, display need to be reviewed.

--- GrandPy_BotApp/static/js/custom.js
+++ GrandPy_BotApp/static/js/custom.js
@@ -10,34 +10,41 @@ $('button').click(function () {
 	// Gather the User's sentence
 	var userRequest = document.getElementById("User_destination");
 	
+	// Call to views.py
 	$.ajax({
 		// Route to /result/ page in views.py
         url: '/result/',
-        // Data to send
+        // Data to send with
         data: {query : userRequest.value},
         // Return accepted format
         dataType: 'json',
         
-        // In case of succes - json_data is return from AJAX GET call
+        // In case of succes - json_data will be returned from AJAX GET call
         success: function(json_data) {
 			// Message
             console.log(json_data);
 
 			// Generate a map through Google Map
-			initMap(json_data.lat,json_data.lng, 'googleMap')
+			generateMap(json_data.lat,json_data.lng, 'googleMap');
+			
+			// Generation of the what-about text
+			generateText(json_data.about, 'mediaWiki');
+			
 			},
 			
 		// In case of error
-        error : function(resultat, statut, erreur){
+        error: function(result, status, error_type){
 			// Message
-			console.log("AJAX (Get) function turn crazy");
+			console.log("AJAX (Get) function turn crazy " + error_type);
 			}
 		});
 	});
 
 
 // Google Map integration in the website
-function initMap(latitude, longitude, IdHTML ) {
+function generateMap(latitude, longitude, IdHTML ) {
+// Generate a Google map from :latitude & :longitude
+// Element will be pinned to the :IdHTML
 
 	// Grab the right id element in the HTML
 	var mapElement = document.getElementById(IdHTML);
@@ -47,4 +54,12 @@ function initMap(latitude, longitude, IdHTML ) {
 		center: {lat: latitude, lng: longitude},
 		zoom: 18
 		});
+}
+
+function generateText(textToDisplay, IdHTML){
+// Generate a text from :textToDisplay
+// Element will be pinned to the :IdHTML
+
+	// Grab the right id element in the HTML and modify text
+	document.getElementById(IdHTML).innerHTML = "Sais-tu que ... " + textToDisplay; 
 }
--- GrandPy_BotApp/views.py
+++ GrandPy_BotApp/views.py
@@ -3,7 +3,7 @@
 from flask import Flask, jsonify, render_template, request
 
 from GrandPy_BotApp.User import User
-from GrandPy_BotApp.MediaWiki import MediaWiki
+from mediawiki import MediaWiki
 from GrandPy_BotApp.GoogleMapAPI import GoogleMapAPI
 
 app = Flask(__name__)
@@ -36,10 +36,29 @@ def result():
 	Place.geocode()
 	
 	# Message
+	print("---MAPS---")
 	print("lat: %2.5f || lng: %2.5f" % (Place.lat,Place.lng))
+	
+	# Generation of a MediaWiki instance
+	wikipedia = MediaWiki()
+	
+	# Set french as language
+	wikipedia.set_api_url(api_url=u'https://fr.wikipedia.org/w/api.php', lang=u'en')
+
+	# Generate the place name from the lat lng
+	json_loc = wikipedia.geosearch(latitude= Place.lat, longitude=Place.lng)
+
+	# Search introduction sentence about the place
+	json_search = wikipedia.opensearch(json_loc[0], results=1)
+	
+	# Message
+	print("---MediaWiki---")
+	print(json_loc[0])
+	print(json_search[0][1])
 	
 	# Return
 	return jsonify(result = Minot.query,
 					lat = Place.lat,
-					lng = Place.lng
+					lng = Place.lng,
+					about = json_search[0][1]
 					)
