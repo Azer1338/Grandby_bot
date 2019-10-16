9dfcb57468db16278fdcc125737d4eaeca014597
Azer1338 <adrien13.f@gmail.com>
2019-10-16 21:48:04 +0200

    Test_parse_tool.py tests added.

--- test_parse_tool.py
+++ test_parse_tool.py
@@ -13,49 +13,50 @@ def test_parsing_method():
     assert result == "  12   avenue  mie d'aghonne 31200  Toulouse"
 
 
-
-
-def test_parsing_error():
+def test_parsing_light_words():
     """ Only low weight words in the query
     """
 
-    query = "abord seraient 12 comparable directement avenue divers mie d'aghonne 31200 huitième Toulouse"
+    query = "allo allons allô alors anterieur a  été être ô"
     result = parsing_method(query)
 
-    assert result == "  12   avenue  mie d'aghonne 31200  Toulouse"
+    assert result == "         "
 
-def test_parsing_error():
+
+def test_parsing_heavy_words():
     """ Only high weight words in the query
     """
 
-    query = "abord seraient 12 comparable directement avenue divers mie d'aghonne 31200 huitième Toulouse"
+    query = "  12   avenue  mie d'aghonne 31200  Toulouse"
     result = parsing_method(query)
 
     assert result == "  12   avenue  mie d'aghonne 31200  Toulouse"
 
-def test_parsing_error():
-    """ Error - Assert not - logique de test inverse - String
-    """
+# def test_parsing_error():
+#     """ Error - Assert not - logique de test inverse - String
+#     """
+#
+#     query = "abord seraient 12 comparable directement avenue divers mie d'aghonne 31200 huitième Toulouse"
+#     result = parsing_method(query)
+#
+#     assert not result == " seraient  12   avenue  mie d'aghonne 31200  Toulouse"
+#
+# def test_parsing_error():
+#     """ Error - Assert not - logique de test inverse - Integer
+#     """
+#
+#     query = "1"
+#     result = parsing_method(query)
+#
+#     assert not result == ""
 
-    query = "abord seraient 12 comparable directement avenue divers mie d'aghonne 31200 huitième Toulouse"
-    result = parsing_method(query)
 
-    assert not result == " seraient  12   avenue  mie d'aghonne 31200  Toulouse"
 
-def test_parsing_error():
-    """ Error - Assert not - logique de test inverse - Integer
-    """
-
-    query = "abord seraient 12 comparable directement avenue divers mie d'aghonne 31200 huitième Toulouse"
-    result = parsing_method(query)
-
-    assert not result == " seraient  12   avenue  mie d'aghonne 31200  Toulouse"
-
-def test_parsing_error():
+def test_parsing_empty_chain():
     """ word chain empty
     """
 
-    query = "abord seraient 12 comparable directement avenue divers mie d'aghonne 31200 huitième Toulouse"
+    query = ""
     result = parsing_method(query)
 
-    assert not result == " seraient  12   avenue  mie d'aghonne 31200  Toulouse"
+    assert result == ""
